cb997233a2aae4f88b34c936e573007ace8b1386
YuriiHorbach <yuriihorbach.career@gmail.com>
2025-08-21 11:00:29 +0200

    Fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_08_21_082536) do
-ActiveRecord::Schema[8.0].define(version: 2025_08_20_205933) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
